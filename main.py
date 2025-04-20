8e88160de022efba53296ab8a9d607aceb0463a3
Abdumalik9900 <Yarmatovabdumalik@gmail.com>
2025-04-20 20:07:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import ta
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TELEGRAM_API_TOKEN = "YOUR_TELEGRAM_API_TOKEN"
+TELEGRAM_API_TOKEN = "TELEGRAM_API_TOKEN"
 
 # Texnik tahlil funktsiyasi
 def swing_trade_analysis(symbol):
